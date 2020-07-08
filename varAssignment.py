24614f54881fd85c63f3aee2638433545e1d65be
sunnys.thakur@gmail.com <sunnys.thakur@gmail.com>
2020-07-08 17:36:12 +0530

    Added Python learning code.

---
+++ varAssignment.py
@@ -0,0 +1,11 @@
+#/usr/bin/python
+
+counter = 0
+miles = 1000.0
+name = 'Sunny'
+counter = counter + 2
+kilometers = 1.609 * miles
+print('%f miles is the same as %f km' % (miles, kilometers))
+
+print(counter)
+print("My name is %s %s" % (name, "Thakur"))
