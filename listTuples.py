d2d6449f2a762af76dc9ec530369a60079a5e4e0
sunnys.thakur@gmail.com <sunnys.thakur@gmail.com>
2020-07-08 21:26:05 +0530

    List Tuples practice

---
+++ listTuples.py
@@ -0,0 +1,21 @@
+#/usr/bin/python
+
+#List Examples
+aList = [1, 2, 3, 4]
+print(aList)
+
+print(aList[0])
+print(aList[2:])
+print(aList[:3])
+
+aList[1] = 5
+print(aList)
+
+#Tuple Examples
+aTuple = ('robots', 77, 93, 'try')
+print(aTuple)
+print(aTuple[:3])
+print(aTuple[1])
+
+aTuple[1] = 5 #Tuples not change the values.
+print(aTuple[1])
