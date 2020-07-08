90bd167e58e6467eb48fdbd4975e83d4b16a9077
sunnys.thakur@gmail.com <sunnys.thakur@gmail.com>
2020-07-08 20:02:19 +0530

    Added Python learning code.

---
+++ pyNumbers.py
@@ -0,0 +1,12 @@
+#/usr/bin/python
+
+#Type of numbers
+#int
+    #long
+    #bool
+#float
+#complex
+
+floatnum = 1.1
+print (floatnum)
+
---
+++ pyStrings.py
@@ -0,0 +1,16 @@
+#/usr/bin/python
+
+pystr = 'Python'
+iscool = 'is cool!'
+print(pystr[0])
+print(pystr[2:5])
+print(iscool[:2])
+print(iscool[3:])
+print(iscool[-1])
+print(pystr + iscool)
+print(pystr + ' ' + iscool)
+print(pystr * 2)
+print('_' * 20)
+pystr1 = '''python
+... is cool'''
+print(pystr1)
