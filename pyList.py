55c1f8a2cb78142f54bac0d42f3248d0d9ef9e81
sunnys.thakur@gmail.com <sunnys.thakur@gmail.com>
2020-07-16 15:35:52 +0530

    study Error Exception Lists in Python

--- pyList.py
+++ pyList.py
@@ -1,11 +1,11 @@
 #/usr/bin/python
 
-squared = [x ** 2 for x in range(4)]
+squared = [x ** 2 for x in range(4)] # square of nu=
 for i in squared:
     print(i)
 
 print("#########################################")
 
-sqdEvens = [x ** 2 for x in range(8) if not x % 2]
+sqdEvens = [x ** 2 for x in range(8) if not x % 2] #square of even numbers in list
 for i in sqdEvens:
     print(i)
